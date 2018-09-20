8d21555df0dd7cee08b7e3e906dca1f13b66acf4
Igor dos Santos <santosigor013@gmail.com>
2018-09-20 13:40:31 -0300

    Add files via upload

--- assets/js/combo-text-search.js
+++ assets/js/combo-text-search.js
@@ -1,45 +1,43 @@
+function openCombo() {
+	$('.ctsCombo .itemList').css('display', 'block');
+  $('#resultCTS').hide(); 
+  $('#ctsCombo').show();
+}
+
 $(document).ready(function() {
 
-  $('#ctsInput, .ctsInput span').on("click", function (e) {
-    e.preventDefault();
-    $('.ctsCombo span').css('display', 'block');
-    $('#ctsCombo').show();
-    $('#resultCTS').hide(); 
-  });
-
-  var submn = $(".clkN");
-  $("body").on("click", function (e) {
-    if (submn.has(e.target).length || e.target == submn[0])
-      return;
-    $('#ctsCombo').hide();
-    $('.ctsCombo span').css('display', 'block');
-  });
-
-  $('.ctsCombo span').on("click", function (e) {
-    e.preventDefault();
-    var txtCTS = $(this).text();
-    $('#ctsInput').val();
-    $('#ctsInput').val(txtCTS);
-    $('.ctsCombo span').css('display', 'block');
-  });
+	$('.itemList').click(function(e) {
+	  e.preventDefault();
+	  var txtCTS = $(this).text();
+	  $('#ctsInput').val();
+	  $('#ctsInput').val(txtCTS);
+	});
 
 });
 
+function closeCombo() {
+	setTimeout(function(){
+		$('.ctsCombo .itemList').css('display', 'block');
+	  $('#resultCTS').hide(); 
+	  $('#ctsCombo').hide();
+	}, 100);
+}
+
 function searchCT() {
   $('#resultCTS').hide(); 
-  var input, filter, div, span, i;
-  input = document.getElementById("ctsInput");
+  var input, filter, div, a, i;
+  input = document.getElementById('ctsInput');
   filter = input.value.toUpperCase();
-  div = document.getElementById("ctsCombo");
-  span = div.getElementsByTagName("span");
-  for (i = 0; i < span.length; i++) {
-    if (span[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      span[i].style.display = "";
+  div = document.getElementById('ctsCombo');
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = '';
     } else {
-      span[i].style.display = "none";
+      a[i].style.display = "none";
     }
   }
-  var visivel = $('.ctsCombo span').is(':visible');
+  var visivel = $('.ctsCombo .itemList').is(':visible');
   if (!visivel) {
     $('#resultCTS').show();
   }
